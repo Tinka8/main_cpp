1e981d51888a4ee9dfed3a442424ff4c22a69321
Tinka8 <lenmatka818@gmail.com>
2022-01-20 15:47:57 +0100

    array

--- ex6.cpp
+++ ex6.cpp
@@ -4,20 +4,21 @@ using namespace std;
 
 int main() {
 
-    int result = 0;
+    int sum = 0;
 
-    string heroNames[10] = {"Tiny", "Legion Commander", "Night Stalker", "Chaos Knight", "Io", "Phoenix", "Magnus", "Snapfire", "Undying", "Tidehunter"};
-    int heroStrengths[10] = {30, 25, 23, 22, 17, 23, 25, 20, 22, 27}; 
+    string heroNames[15] = {"Tiny", "Legion Commander", "Night Stalker", "Chaos Knight", "Io", "Phoenix", "Magnus", "Snapfire", "Undying", "Tidehunter", "Abadon", "Dawnbreaker", "Axe", "Huskar", "Marci"};
+    int heroStrengths[15] = {30, 25, 23, 22, 17, 23, 25, 20, 22, 27, 22, 26, 25, 21, 23}; 
 
-    // sum strengths
-    int sum = heroStrengths[0] + heroStrengths[1] + heroStrengths[2] + heroStrengths[3] + heroStrengths[4] + heroStrengths[5] + heroStrengths[6] + heroStrengths[7] + heroStrengths[8] + heroStrengths[9];
-    
+    // sum strengths 
+    for(int i = 0; i < 15; i++) {
+        sum = sum + heroStrengths[i];
+
+    }
+
     // avg strength
-    float average = sum / 10.0;
+    float average = sum / 15.0;
 
-
-    for(int i = 0; i < 10; i++) {
-        heroStrengths[i];
+    for(int i = 0; i < 15; i++) {
         cout << heroNames[i] << ": " << heroStrengths[i] << endl;
 
     }
@@ -28,11 +29,12 @@ int main() {
 
     cout << endl;
     cout << "Heroes with strength higher than average:" << endl;
-    cout << heroNames[0] << ": " << heroStrengths[0] << endl;
-    cout << heroNames[1] << ": " << heroStrengths[1] << endl;
-    cout << heroNames[5] << ": " << heroStrengths[5] << endl;
-    cout << heroNames[6] << ": " << heroStrengths[6] << endl;
-    cout << heroNames[9] << ": " << heroStrengths[9] << endl;
+    for(int j = 0; j < 15; j++) {
+        if(average < heroStrengths[j]) {
+            cout << heroNames[j] << ": " << heroStrengths[j] << endl;
+        }
+    }
+
 
     return 0;
 }
